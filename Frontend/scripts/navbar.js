4f5501cebb4c5ee6dcb32c6a62cb522c4b2d288d
Priyanshii23 <agrawalpriyanshi123@gmail.com>
2025-01-01 14:25:35 +0530

    Nav Bar Added

---
+++ Frontend/scripts/navbar.js
@@ -0,0 +1,31 @@
+const navbar =   `
+          <div id="nav1">
+            <div id="navleft">
+                <img src="/Cuddle care/CuddleCarelogo.png" alt="Cuddle Care Logo">
+                <hr>
+                <div id="deliverto">
+                    Delivery to
+
+                    <select name="" id="deliver">
+                        <option value=""></option>
+                    </select>
+                </div>
+            </div>
+
+            <div id="nav-right">
+                <button>Download App</button>
+                <a href="login.html"> Hello, Log in</a>
+                <a href="offer.html">Offers</a>
+                <a href="cart.html">Cart</a>
+            </div>
+        </div>
+        <div id="nav2">
+            <a href="category.html">Categories</a>
+            <a href="health.html">Health</a>
+            <a href="food.html">Food</a>
+            <a href="fashion.html">Fashion</a>
+            <a href="bath.html">Bath</a>
+        </div>`
+
+
+document.getElementById("nav").innerHTML = navbar
